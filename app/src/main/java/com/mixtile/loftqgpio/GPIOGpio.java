103b93a19a405fecc4005ec820723f21ad91959b
Phil Han <pengphei@sina.com>
2015-07-18 18:57:00 +0800

    add reading test routines

--- app/src/main/java/com/mixtile/loftqgpio/GPIOGpio.java
+++ app/src/main/java/com/mixtile/loftqgpio/GPIOGpio.java
@@ -9,9 +9,17 @@ public class GPIOGpio {
     static int j4_00 = 0;
     static int j4_01 = 1;
     static int j4_02 = 2;
+    static int j4_03 = 3;
+
+    static int j4_06 = 6;
+    static int j4_07 = 7;
+    static int j4_08 = 8;
+    static int j4_09 = 9;
     GPIOLoftq wrapper = new GPIOLoftq();
 
     public void init(){
+
+        // enable output gpios
         wrapper.export(gpiozone, j4_00);
         wrapper.enable_out(gpiozone, j4_00);
 
@@ -20,25 +28,76 @@ public class GPIOGpio {
 
         wrapper.export(gpiozone, j4_02);
         wrapper.enable_out(gpiozone, j4_02);
+
+        wrapper.export(gpiozone, j4_03);
+        wrapper.enable_out(gpiozone, j4_03);
+
+        // enable input gpios
+
+        wrapper.export(gpiozone, j4_06);
+        wrapper.enable_in(gpiozone, j4_06);
+
+        wrapper.export(gpiozone, j4_07);
+        wrapper.enable_in(gpiozone, j4_07);
+
+        wrapper.export(gpiozone, j4_08);
+        wrapper.enable_in(gpiozone, j4_08);
+
+        wrapper.export(gpiozone, j4_09);
+        wrapper.enable_in(gpiozone, j4_09);
+
+
     }
 
-    public void set_01(int value){
+    public void set_00(int value){
 
         wrapper.write(gpiozone, j4_00, value);
     }
 
-    public void set_02(int value){
+    public void set_01(int value){
 
         wrapper.write(gpiozone, j4_01, value);
     }
 
-    public void set_03(int value){
+    public void set_02(int value){
+
         wrapper.write(gpiozone, j4_02, value);
+    }
+
+    public void set_03(int value){
+
+        wrapper.write(gpiozone, j4_03, value);
+    }
+
+    public int get_06(){
+
+        return wrapper.read(gpiozone, j4_06);
+    }
+
+    public int get_07(){
+
+        return wrapper.read(gpiozone, j4_07);
+    }
+
+    public int get_08(){
+
+        return wrapper.read(gpiozone, j4_08);
+    }
+
+    public int get_09(){
+
+        return wrapper.read(gpiozone, j4_09);
     }
 
     public void fini(){
         wrapper.unexport(gpiozone, j4_00);
         wrapper.unexport(gpiozone, j4_01);
         wrapper.unexport(gpiozone, j4_02);
+        wrapper.unexport(gpiozone, j4_03);
+
+        wrapper.unexport(gpiozone, j4_06);
+        wrapper.unexport(gpiozone, j4_07);
+        wrapper.unexport(gpiozone, j4_08);
+        wrapper.unexport(gpiozone, j4_09);
     }
 }
--- app/src/main/java/com/mixtile/loftqgpio/MainActivity.java
+++ app/src/main/java/com/mixtile/loftqgpio/MainActivity.java
@@ -3,16 +3,32 @@ package com.mixtile.loftqgpio;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
-
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG="GPIOMain";
+
+    private ToggleButton tgbt0 = null;
     private ToggleButton tgbt1 = null;
     private ToggleButton tgbt2 = null;
     private ToggleButton tgbt3 = null;
+
+    private ToggleButton rtgbt6 = null;
+    private ToggleButton rtgbt7 = null;
+    private ToggleButton rtgbt8 = null;
+    private ToggleButton rtgbt9 = null;
+
+    private Button rbt6 = null;
+    private Button rbt7 = null;
+    private Button rbt8 = null;
+    private Button rbt9 = null;
+
     private GPIOGpio iowrapper = new GPIOGpio();
 
     @Override
@@ -20,17 +36,37 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tgbt1 = (ToggleButton)findViewById(R.id.toggleButton1);
-        tgbt2 = (ToggleButton)findViewById(R.id.toggleButton2);
-        tgbt3 = (ToggleButton)findViewById(R.id.toggleButton3);
+        tgbt0 = (ToggleButton)findViewById(R.id.toggleButton1);
+        tgbt1 = (ToggleButton)findViewById(R.id.toggleButton2);
+        tgbt2 = (ToggleButton)findViewById(R.id.toggleButton3);
+        tgbt3 = (ToggleButton)findViewById(R.id.toggleButton4);
+
+        rtgbt6 = (ToggleButton)findViewById(R.id.rToggleJ406);
+        rtgbt7 = (ToggleButton)findViewById(R.id.rToggleJ407);
+        rtgbt8 = (ToggleButton)findViewById(R.id.rToggleJ408);
+        rtgbt9 = (ToggleButton)findViewById(R.id.rToggleJ409);
+
+        rbt6 = (Button)findViewById(R.id.rButtonJ406);
+        rbt7 = (Button)findViewById(R.id.rButtonJ407);
+        rbt8 = (Button)findViewById(R.id.rButtonJ408);
+        rbt9 = (Button)findViewById(R.id.rButtonJ409);
 
         iowrapper.init();
 
-        tgbt1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        tgbt0.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                tgbt0.setChecked(isChecked);
+                iowrapper.set_00(isChecked ? 1 : 0);
+            }
+
+        });
+
+        tgbt1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                 tgbt1.setChecked(isChecked);
-                iowrapper.set_01(isChecked ? 1:0);
+                iowrapper.set_01(isChecked ? 1 : 0);
             }
 
         });
@@ -39,7 +75,7 @@ public class MainActivity extends ActionBarActivity {
 
             public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                 tgbt2.setChecked(isChecked);
-                iowrapper.set_01(isChecked ? 1:0);
+                iowrapper.set_02(isChecked ? 1 : 0);
             }
 
         });
@@ -48,9 +84,41 @@ public class MainActivity extends ActionBarActivity {
 
             public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
                 tgbt3.setChecked(isChecked);
-                iowrapper.set_01(isChecked ? 1:0);
+                iowrapper.set_03(isChecked ? 1 : 0);
             }
 
+        });
+
+        rbt6.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int value = iowrapper.get_06();
+                Log.i(TAG, "J406 read " + value);
+                rtgbt6.setChecked(value == 1);
+            }
+        });
+
+        rbt7.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                int value = iowrapper.get_07();
+                Log.i(TAG, "J407 read " + value);
+                rtgbt7.setChecked(value==1);
+            }
+        });
+
+        rbt8.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                int value = iowrapper.get_08();
+                Log.i(TAG, "J408 read " + value);
+                rtgbt8.setChecked(value == 1);
+            }
+        });
+
+        rbt9.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                int value = iowrapper.get_09();
+                Log.i(TAG, "J408 read " + value);
+                rtgbt9.setChecked(value == 1);
+            }
         });
     }
 
@@ -79,5 +147,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        iowrapper.fini();
     }
 }
